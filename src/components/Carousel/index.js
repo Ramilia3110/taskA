54c4178063d86927ec3da6d5d49d5eec4d21b7ed
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2023-05-08 23:25:58 +0300

    add carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { EffectCoverflow, Pagination, Navigation } from "swiper";
+
 import g1 from "./../../assets/images/1.jpg";
 import g2 from "./../../assets/images/2.jpg";
 import g3 from "./../../assets/images/3.jpg";
@@ -14,74 +23,63 @@ import g12 from "./../../assets/images/12.jpg";
 import "./styles.scss";
 
 export const Carousel = () => {
-  const leftPerspectives = [
-    { x: -10, z: -4 },
-    { x: -20, z: -8 },
-    { x: -30, z: -12 },
-    { x: -40, z: -16 },
-    { x: -50, z: -20 },
-    { x: 10, z: -4 },
-  ];
-
-  const rightPerspectives = [
-    { x: 10, z: -4 },
-    { x: 20, z: -8 },
-    { x: 30, z: -12 },
-    { x: 40, z: -16 },
-    { x: 50, z: -20 },
-    { x: -10, z: -4 },
-  ];
-
-  const leftCards = document.querySelectorAll(".left .item");
-  const rightCards = document.querySelectorAll(".right .item");
-
-  const translateImage = (target, p) => {
-    target.style.transform = `translate3d(${p.x}rem, 0, ${p.z}rem)`;
-  };
-
-  const animateCards = (c, perspectives) => {
-    const count = parseInt(c.dataset.counter);
-    translateImage(c, perspectives[count - 1]);
-    c.dataset.counter = (count === 6 ? 1 : count + 1).toString();
-  };
-
-  const loop = () => {
-    setInterval(() => {
-      leftCards.forEach((c) => {
-        animateCards(c, leftPerspectives);
-      });
-
-      rightCards.forEach((c) => {
-        animateCards(c, rightPerspectives);
-      });
-    }, 1000);
-  };
-
-  loop();
-
   return (
     <div className="carousel">
-      <div className="gallery">
-        <div className="left">
-          <div className="inner">
-            <img className="item" srs={g1} data-counter="1" />
-            <img className="item" src={g2} data-counter="2" />
-            <img className="item" src={g3} data-counter="3" />
-            <img className="item" src={g4} data-counter="4" />
-            <img className="item" src={g5} data-counter="5" />
-            <img className="item" src={g6} data-counter="6" />
+      <div className="container">
+        <h1 className="heading">Flower Gallery</h1>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
+          pagination={{ el: ".swiper-pagination", clickable: true }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiper_container"
+        >
+          <SwiperSlide>
+            <img src={g1} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g2} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g3} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g4} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g5} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g6} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={g7} alt="slide_image" />
+          </SwiperSlide>
+
+          <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
           </div>
-        </div>
-        <div className="right">
-          <div className="inner">
-            <img className="item" srs={g7} data-counter="1" />
-            <img className="item" src={g8} data-counter="2" />
-            <img className="item" src={g9} data-counter="3" />
-            <img className="item" src={g10} data-counter="4" />
-            <img className="item" src={g11} data-counter="5" />
-            <img className="item" src={g12} data-counter="5" />
-          </div>
-        </div>
+        </Swiper>
       </div>
     </div>
   );
