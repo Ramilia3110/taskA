f2c9644c7cebf5a3858e2e60cb8a80b3a4cfae20
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2023-05-11 19:10:12 +0300

    add banner

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,86 +1,16 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { EffectCoverflow, Pagination, Navigation } from "swiper";
-
-import g1 from "./../../assets/images/1.jpg";
-import g2 from "./../../assets/images/2.jpg";
-import g3 from "./../../assets/images/3.jpg";
-import g4 from "./../../assets/images/4.jpg";
-import g5 from "./../../assets/images/5.jpg";
-import g6 from "./../../assets/images/6.jpg";
-import g7 from "./../../assets/images/7.jpg";
-import g8 from "./../../assets/images/8.jpg";
-import g9 from "./../../assets/images/9.jpg";
-import g10 from "./../../assets/images/10.jpg";
-import g11 from "./../../assets/images/11.jpg";
-import g12 from "./../../assets/images/12.jpg";
 import "./styles.scss";
 
 export const Carousel = () => {
   return (
     <div className="carousel">
-      <div className="container">
-        <h1 className="heading">Flower Gallery</h1>
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          loop={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
-          pagination={{ el: ".swiper-pagination", clickable: true }}
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-            clickable: true,
-          }}
-          modules={[EffectCoverflow, Pagination, Navigation]}
-          className="swiper_container"
-        >
-          <SwiperSlide>
-            <img src={g1} alt="slide_image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={g2} alt="slide_image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={g3} alt="slide_image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={g4} alt="slide_image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={g5} alt="slide_image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={g6} alt="slide_image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={g7} alt="slide_image" />
-          </SwiperSlide>
-
-          <div className="slider-controler">
-            <div className="swiper-button-prev slider-arrow">
-              <ion-icon name="arrow-back-outline"></ion-icon>
-            </div>
-            <div className="swiper-button-next slider-arrow">
-              <ion-icon name="arrow-forward-outline"></ion-icon>
-            </div>
-            <div className="swiper-pagination"></div>
-          </div>
-        </Swiper>
-      </div>
+      <div className="slide">bjh</div>
+      <div className="slide"></div>
+      <div className="slide"></div>
+      <div className="slide hover "></div>
+      <div className="slide"></div>
+      <div className="slide"></div>
+      <div className="slide"> </div>
     </div>
   );
 };
